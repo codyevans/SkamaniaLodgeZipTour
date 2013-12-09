3ae3df230d59463244c7d5e5db931c516c7b50f7
Cody Evans <codyevansdesign@gmail.com>
2013-12-09 15:27:32 -0700

    cache break - js

--- index.php
+++ index.php
@@ -646,7 +646,7 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
         <script src="js/plugins.min.js?ver1.2"></script>
-        <script src="js/main.js?ver=1.7"></script>
+        <script src="js/main.js?ver=1.8"></script>
        
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
         <script>

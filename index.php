687c88472d91a509296ca78930ad30a757b7763c
Cody Evans <codyevansdesign@gmail.com>
2014-02-11 17:43:21 -0700

    cache break

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
         <meta name="apple-mobile-web-app-capable" content="yes" />
 
-        <link rel="stylesheet" href="css/build/global-prefixed.css?ver=1.1">
+        <link rel="stylesheet" href="css/build/global-prefixed.css?ver=1.2">
 
         <style type="text/css">
             .royalSlider {
@@ -582,7 +582,7 @@
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
-        <script src="js/build/production.min.js?ver1.1"></script>
+        <script src="js/build/production.min.js?ver1.2"></script>
        
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
         <script>

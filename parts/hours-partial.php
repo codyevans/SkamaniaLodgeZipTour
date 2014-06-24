3d469ba2993da2a987d72577a0da1b850278963c
Cody Evans <codyevansdesign@gmail.com>
2014-06-24 11:35:23 -0600

    adjusted tour times

--- parts/hours-partial.php
+++ parts/hours-partial.php
@@ -7,17 +7,24 @@
          </h2>
         
          <p class="emp" style="line-height: 21px;">
-            <strong>Office hours:</strong> Sunday - Saturday from 9:00am - 5:00pm
+            <strong>Office hours:</strong> 8:30am - 5:30pm Daily
         </p>
         <hr>
-        <h3 style="margin-bottom: 0;">Tour Times:</h3>
+        <h3 style="margin-bottom: 5px;">Tour Times:</h3>
+        <p>
+            <strong>Monday - Friday:</strong> <br>
+            10:00am, 11:00am, 1:00pm, 2:00pm, 4:00pm, 5:00pm
+        </p>
+        <p>
+            <strong>Saturday - Sunday:</strong> <br>
+            9:00am, 10:00am, 11:00am, 12:00pm, 1:00pm, 2:00pm, 3:00pm, 4:00pm, 5:00pm
+        </p>
         <p style="margin-bottom: 30px;">
-            For available tour times please click on the book now button, if your desired 
-            tour time is not listed please call our office at <strong tel="509-427-0202">(509)427-0202</strong>. 
+            For reservations <strong>Book Online</strong> or call our office at <strong tel="509-427-0202">(509)427-0202</strong>. 
         </p>
         <div class="xola-checkout xola-custom" id="533c55dcca64e48f38be6260">
             <a class="bookings-link">
-                 <div class="col-1-1 grid-pad-1-3 first" title="click for online reservations">   
+                 <div class="col-1-1 grid-pad-1-3 first" title="click for online reservations">
                     <div class="res_booking">
                         <p>
                             Book Now &rarr; 

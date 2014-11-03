f78c42065f3dfdb8d4ddadce55c9adae0a463848
Cody Evans <codyevansdesign@gmail.com>
2014-11-03 14:16:31 -0700

    Winter Sched 2014

--- parts/hours-partial.php
+++ parts/hours-partial.php
@@ -3,21 +3,27 @@
     <span class="close-overlay" title="close"></span>
     <div>
          <h2>
-           Operating Hours
+           Operating Hours <span style="font-size: 1rem;">- Winter Schedule</span>
          </h2>
         
          <p class="emp" style="line-height: 21px;">
-            <strong>Office hours:</strong> 8:30am - 5:30pm Daily
+            <strong>Office hours:</strong>  <br> 8:30am - 5:30pm Daily
+        </p>
+        <p class="emp" style="line-height: 21px; margin-top: 15px; background: rgb(48, 76, 88); padding: 10px; text-shadow: none; color: white;">
+            <strong>Office Closed:</strong> November 26th - 27th | December 22nd - 26th
         </p>
         <hr>
         <h3 style="margin-bottom: 5px;">Tour Times:</h3>
         <p>
-            <strong>Monday - Friday:</strong> <br>
-            10:00am, 11:00am, 1:00pm, 2:00pm, 4:00pm, 5:00pm
+            <strong>Monday - Sunday:</strong> <br>
+            10:00am, 1:00pm <br>
+            <span>*Other times may be available by reservation only.</span>
         </p>
+        <h3 style="margin-bottom: 5px;">Winter Pricing:</h3>
         <p>
-            <strong>Saturday - Sunday:</strong> <br>
-            9:00am, 10:00am, 11:00am, 12:00pm, 1:00pm, 2:00pm, 3:00pm, 4:00pm, 5:00pm
+            <strong>November:</strong> $89 + Tax <br>
+            <strong>December - January:</strong> $79 + Tax <br>
+            <strong>February:</strong> $89 + Tax
         </p>
         <p style="margin-bottom: 30px;">
             For reservations <strong>Book Online</strong> or call our office at <strong tel="509-427-0202">(509)427-0202</strong>. 
